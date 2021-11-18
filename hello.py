a9b0abdf148e84d57821c1b8794ed0a13ab6e3c0
むずでょ <muzudho1@gmail.com>
2021-11-18 23:07:18 +0900

    request

--- hello.py
+++ hello.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/hello')
-def hello():
-    return '<img src="static/img/20211102shogi18.png" />'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return '''<html>
+<head>
+    <title>電子政府きふわらべ</title>
+</head>
+<body>
+    <img src="static/img/20211102shogi18.png"/><br/>
+    ok
+</body>
+</html>
+'''
+        pass
+    else:
+        return '''<html>
+<head>
+    <title>電子政府きふわらべ</title>
+</head>
+<body>
+    <img src="static/img/20211102shogi18.png"/><br/>
+    <form method="POST">
+        <input type="text" name="bestmove"/>
+        <input type="submit" value="Vote!"/>
+    </form>
+</body>
+</html>
+'''
