ab8583c65035b105207681c9ada75cbe44b84832
むずでょ <muzudho1@gmail.com>
2021-11-18 23:13:21 +0900

    html

--- hello.py
+++ hello.py
@@ -7,16 +7,4 @@ def index():
     if request.method == 'POST':
         return render_template('voted.html', bestmove=request.form['bestmove'])
     else:
-        return '''<html>
-<head>
-    <title>電子政府きふわらべ</title>
-</head>
-<body>
-    <img src="static/img/20211102shogi18.png"/><br/>
-    <form method="POST">
-        <input type="text" name="bestmove"/>
-        <input type="submit" value="Vote!"/>
-    </form>
-</body>
-</html>
-'''
+        return render_template('vote.html')
