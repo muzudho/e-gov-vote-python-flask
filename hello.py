3ad4a05ddfca5e55285fd9ee8d604373a8ecd976
むずでょ <muzudho1@gmail.com>
2021-11-18 23:00:52 +0900

    img

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/hello')
+def hello():
+    return '<img src="static/img/20211102shogi18.png" />'
